f8e78a69524cb55bed0f86e72881b32b0b529510
MayaCR <106397404+MayaCR@users.noreply.github.com>
2024-04-02 14:54:28 -0400

    Link nav buttons to pages

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('Connected')
+import createMenu from './menu'
+import createAbout from './about'
+import createContact from './contact'
+
+const menuButton = document.getElementById('menu')
+const aboutButton = document.getElementById('about')
+const contactButton = document.getElementById('contact')
+
+menuButton.addEventListener('click', () => {
+	createMenu()
+})
+
+aboutButton.addEventListener('click', () => {
+	createAbout()
+})
+
+contactButton.addEventListener('click', () => {
+	createContact()
+})
+
+// loads web app with menu(home) from start
+createMenu()
